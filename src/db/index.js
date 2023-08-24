9df7288db0cd9e5f8c51ecdfca72746a2bcb2b46
seniorsardorbek <sardorbekmusilman@gmail.com>
2023-08-24 13:43:08 +0500

    db index

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,22 @@
-const mongoose = require('mongoose');
-const config = require('../shared/config');
+const mongoose = require("mongoose");
+const config = require("../shared/config");
 
 module.exports = function () {
   return mongoose
-    .connect( `mongodb+srv://sardorbekmusilman:Just_password03@cluster0.ysxkkxu.mongodb.net/?retryWrites=true&w=majority`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      `mongodb+srv://sardorbekmusilman:Just_password03@cluster0.ysxkkxu.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+        serverSelectionTimeoutMS: 150000, // Increase timeout to 15 seconds
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => {
-      console.log('DB ga ulandi.');
+      console.log("DB ga ulandi.");
     })
     .catch((err) => {
-      console.log('DB da xatolik: ', err);
+      console.log("DB da xatolik: ", err);
     });
 };
